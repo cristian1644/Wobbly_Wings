d891cf5dc130beeef4924d7ee57c601bbe382544
Cristian44 <cristianborioni44@gmail.com>
2025-01-14 09:18:47 +0100

    -aggiunta di 2 ulteriori personaggi -fixato bug pulsante menu scelta personaggio, ora si attiva dopo che si cambia character

--- Wobbly_Wings/Assets/Scripts/CharacterSelectMenu.cs
+++ Wobbly_Wings/Assets/Scripts/CharacterSelectMenu.cs
@@ -5,9 +5,13 @@ public class CharacterSelectMenu : MonoBehaviour
 {
     public Player player; // Riferimento al giocatore
     public Sprite[] birdSprites; // Sprite per il personaggio "Bird"
-    public Sprite[] beeSprites; // Sprite per il personaggio "Cat"
+    public Sprite[] blueBirdSprites;
+    public Sprite[] orangeBirdSprites;
+    public Sprite[] beeSprites; 
     public Button birdButton; // Pulsante per selezionare il personaggio "Bird"
-    public Button beeButton; // Pulsante per selezionare il personaggio "Cat"
+    public Button blueBirdButton;
+    public Button orangeBirdButton;
+    public Button beeButton; 
     public GameObject characterMenuButton;
     public GameObject characterSelectPanel; // Pannello del menu di selezione personaggio
 
@@ -16,6 +20,8 @@ public class CharacterSelectMenu : MonoBehaviour
     {
         // Associa i metodi di selezione ai pulsanti
         birdButton.onClick.AddListener(SelectBird);
+        blueBirdButton.onClick.AddListener(SelectBlueBird);
+        orangeBirdButton.onClick.AddListener(SelectOrangeBird);
         beeButton.onClick.AddListener(SelectBee);
     }
 
@@ -24,13 +30,28 @@ public class CharacterSelectMenu : MonoBehaviour
     {
         player.ChangeCharacter(birdSprites); // Cambia gli sprite del personaggio con quelli del "Bird"
         characterSelectPanel.SetActive(false); // Nascondi il menu dopo la selezione
+        characterMenuButton.SetActive(true);
     }
 
-    // Funzione per selezionare il personaggio "Cat"
+    void SelectBlueBird()
+    {
+        player.ChangeCharacter(blueBirdSprites); // Cambia gli sprite del personaggio con quelli del "Bird"
+        characterSelectPanel.SetActive(false); // Nascondi il menu dopo la selezione
+        characterMenuButton.SetActive(true);
+    }
+
+    void SelectOrangeBird()
+    {
+        player.ChangeCharacter(orangeBirdSprites); // Cambia gli sprite del personaggio con quelli del "Bird"
+        characterSelectPanel.SetActive(false); // Nascondi il menu dopo la selezione
+        characterMenuButton.SetActive(true);
+    }
+
     void SelectBee()
     {
         player.ChangeCharacter(beeSprites); // Cambia gli sprite del personaggio con quelli del "Cat"
         characterSelectPanel.SetActive(false); // Nascondi il menu dopo la selezione
+        characterMenuButton.SetActive(true);
     }
 
     // Funzione per aprire il menu
